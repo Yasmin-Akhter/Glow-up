b0c248169d7af325e929af56d4dbbec59bce148e
Yasmin Akhter <yasminbd89@yahoo.com>
2022-04-05 17:09:10 +0600

    blog added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -3,7 +3,10 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div>
-            <p>blogs..</p>
+            <h1>What is Semantic Tag?</h1>
+            <p>Semantic tag means tag with meaning.That means those HTML tag which are easily understandable by its name is  called semantic tag.semantic tags make it clear to the browser what the meaning of a page and its content is. That clarity is also communicated with search engines, ensuring that the right pages are delivered for the right queries. <br></br>
+                <strong> Examples: </strong>form,table,header etc.
+            </p>
         </div>
     );
 };
